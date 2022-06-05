bd918c45382c70e5a6a94e161f00509039e6df58
Udoy Soumik <salman.soumik@augmedix.com>
2022-06-06 01:35:52 +0600

    Initial Commit

---
+++ TryBGTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TryBGTask
+//
+//  Created by Salman Soumik on 6/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
